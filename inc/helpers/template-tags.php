e748cd2e25b72a713bf8f6dd1e2a78dfc4ba409e
Hassaan Faridi <fireside.faridi@gmail.com>
2024-07-12 18:13:05 +0500

    Aquila blog page: added post title, excerpt, meta values, and read more button

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -21,3 +21,75 @@ function get_the_post_custom_thumbail($post_id, $size = 'featured-image', $addit
 function the_post_custom_thumbnail($post_id, $size = 'featured-thubmnail', $additional_attributes = []) {
     echo get_the_post_thumbnail( $post_id, $size, $additional_attributes );
 }
+
+
+function aquila_posted_on() {
+
+	$year                        = get_the_date( 'Y' );
+	$month                       = get_the_date( 'n' );
+	$day                         = get_the_date( 'j' );
+	$post_date_archive_permalink = get_day_link( $year, $month, $day );
+
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+	// Post is modified ( when post published time is not equal to post modified time )
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( DATE_W3C ) ),
+		esc_attr( get_the_date() ),
+		esc_attr( get_the_modified_date( DATE_W3C ) ),
+		esc_attr( get_the_modified_date() )
+	);
+
+	$posted_on = sprintf(
+		esc_html_x( 'Posted on %s', 'post date', 'aquila' ),
+		'<a href="' . esc_url( $post_date_archive_permalink ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+	echo '<span class="posted-on text-secondary">' . $posted_on . '</span>';
+}
+
+function aquila_posted_by() {
+	$byline = sprintf(
+		esc_html_x( ' by %s', 'post author', 'aquila' ),
+		'<span class="author vcard"><a href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+
+	echo '<span class="byline text-secondary">' . $byline . '</span>';
+}
+
+function aquila_the_excerpt( $trim_character_count = 0 ) {
+	$post_ID = get_the_ID();
+
+	if ( empty( $post_ID ) ) {
+		return null;
+	}
+
+	if ( has_excerpt() || 0 === $trim_character_count ) {
+		the_excerpt();
+
+		return;
+	}
+
+	$excerpt = wp_html_excerpt( get_the_excerpt( $post_ID ), $trim_character_count, '[...]' );
+
+
+	echo $excerpt;
+}
+
+
+
+function aquila_excerpt_more( $more = '' ) {
+
+	if ( ! is_single() ) {
+		$more = sprintf( '<a class="aquila-read-more text-white mt-3 btn btn-info" href="%1$s">%2$s</a>',
+			get_permalink( get_the_ID() ),
+			__( 'Read more', 'aquila' )
+		);
+	}
+
+	return $more;
+}
